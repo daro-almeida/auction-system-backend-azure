e2ac5dce139431cffaf9d9240fb81dc83c411f77
daro.almeida <daro.almeida@campus.fct.unl.pt>
2022-10-14 10:49:18 +0100

    converted UserJSON.java to record

--- src/scc/data/JSON/UserJSON.java
+++ src/scc/data/JSON/UserJSON.java
@@ -2,28 +2,4 @@ package scc.data.JSON;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class UserJSON {
-
-    public final String nickname;
-    public final String name;
-    public final String password;
-    public final String imageBase64;
-
-    public UserJSON(@JsonProperty("nickname") String nickname, @JsonProperty("name") String name,
-                    @JsonProperty("password") String password, @JsonProperty("imageBase64") String imageBase64) {
-        this.nickname = nickname;
-        this.name = name;
-        this.password = password;
-        this.imageBase64 = imageBase64;
-    }
-
-    @Override
-    public String toString() {
-        return "UserJSON{" +
-                "nickname='" + nickname + '\'' +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", imageBase64='" + imageBase64 + '\'' +
-                '}';
-    }
-}
+public record UserJSON(String nickname, String name, String password, String imageBase64) { }
