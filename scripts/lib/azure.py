fda3ee2b22533bf736d1e0f393dd6c548d7077e0
diogo464 <diogo464@protonmail.com>
2022-11-24 16:33:27 +0000

    removed azure functions

--- scripts/lib/azure.py
+++ scripts/lib/azure.py
@@ -3,23 +3,3 @@ import os
 
 REGION = "westeurope"
 RESOURCE_GROUP = "scc-backend-d464-rg"
-
-
-def list_function_names() -> list[str]:
-    return [
-        name.removeprefix("function-")
-        for name in os.listdir("modules")
-        if re.match(r"^function-[a-z][a-z0-9-]*$", name)
-    ]
-
-
-def function_path(function_name: str) -> str:
-    return f"modules/{function_directory_name(function_name)}"
-
-
-def function_artifact_id(name: str) -> str:
-    return f"scc-backend-azure-function-{name}"
-
-
-def function_directory_name(name: str) -> str:
-    return f"function-{name}"
