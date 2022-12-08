1d3ac7fb4976581b77fa22e9c7ae4c3a1cda2ff6
diogo464 <diogo464@protonmail.com>
2022-12-08 17:21:34 +0000

    update

--- deploy/dockerfiles/tester.dockerfile
+++ deploy/dockerfiles/tester.dockerfile
@@ -1,6 +1,5 @@
 FROM artilleryio/artillery
-WORKDIR /usr/local/tester
-ARG yml
-COPY artillery.sh artillery.sh
-ADD testing/artillery/ testing/artillery/
-ENTRYPOINT ["./artillery.sh", "$yml"]
+WORKDIR /usr/local/app
+RUN npm install @faker-js/faker --save-dev
+ADD testing/artillery/*.yml .
+ADD testing/artillery/*.js .
--- tester/create-tester-image.sh
+++ tester/create-tester-image.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-#arg_example: workload1.yml
-docker build -t git.d464.sh/diogo464/scc-tester:latest -f deploy/dockerfiles/tester.dockerfile --build-arg yml=$@ ./tester
+docker build -t git.d464.sh/diogo464/scc-tester:latest -f deploy/dockerfiles/tester.dockerfile ./tester
 docker push git.d464.sh/diogo464/scc-tester:latest
